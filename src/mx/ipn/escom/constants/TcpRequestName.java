16dd2629d84abe1b905453e2e66e8b9a53178bd8
vicleo14 <vicleo.morales@hotmail.com>
2019-03-17 08:07:46 -0600

    Client implementation

---
+++ src/mx/ipn/escom/constants/TcpRequestName.java
@@ -0,0 +1,9 @@
+package mx.ipn.escom.constants;
+
+public class TcpRequestName {
+	public static final int NEW_FORUM=1;
+	public static final int NEW_COMMENT=2;
+	public static final int GET_FORUM=3;
+	public static final int LOOK_FOR_FORUM=4;
+	public static final int AUTHENTICATE_USER=5;
+}
---
+++ src/mx/ipn/escom/sockets/TcpClientSocket.java
@@ -0,0 +1,5 @@
+package mx.ipn.escom.sockets;
+
+public class TcpClientSocket {
+
+}
